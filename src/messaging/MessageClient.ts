1d92d0d3365a2784ac866cf4b54bacb609c2f2aa
Toby Chambers <74092151+thepiex@users.noreply.github.com>
2023-04-03 22:02:14 +0100

    Initialise ESLint

--- src/messaging/MessageClient.ts
+++ src/messaging/MessageClient.ts
@@ -1,74 +1,63 @@
-import { BaseClass } from "../BaseClass";
-import { Gateway } from "../Gateway";
-import { connect } from 'amqplib/callback_api'
+import { BaseClass } from '../BaseClass'
+import type { Gateway } from '../Gateway'
+import { connect } from 'amqplib/'
 
 export class MessageClient extends BaseClass {
-    client: Gateway;
-    connection: any;
-    channel: any;
-    options: any;
-    constructor(client: Gateway) {
-        super();
+  client: Gateway
+  connection: any
+  channel: any
+  options: any
+  constructor (client: Gateway) {
+    super()
 
-        this.client = client;
+    this.client = client
 
-        this.options = client.options.rabbit
+    this.options = client.options.rabbit
 
-        this.connection = null;
+    this.connection = null
 
-        this.channel = null;
+    this.channel = null
+  }
 
-    }
+  async connect (): Promise<void> {
+    // Create a connection with the AMQP Server,
+    this.connection = await connect(this.options.hostname)
 
-    async connect() {
+    // Create a channel witin the connection,
+    this.channel = await this.connection.createChannel((err: any) => {
+      // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+      if (err) {
+        console.log('Gateway - RabbitMQ Channel Creation Error')
+        return
+      }
 
-        // Create a connection with the AMQP Server,
-        connect(this.options.hostname, (err, connection) => {
+      // Create the exchange if it does not exist, and then bind a temporary channel to it,
+      this.channel.assertExchange('secondary', 'fanout', { durable: false })
 
-            if (err) {
-                console.log('AMP CONNECTION ERROR');
-                return;
-            }
-            this.connection = connection
-
-
-            // Create a channel witin the connection,
-            this.channel = connection.createChannel((err, channel) => {
-                if (err) {
-                    console.log('Gateway - RabbitMQ Channel Creation Error')
-                    return;
-                }
-
-
-                // Create the exchange if it does not exist, and then bind a temporary channel to it,
-                this.channel.assertExchange('secondary', 'fanout', { durable: false })
-
-                this.channel.assertQueue('', { exclusive: true }, (err, q) => {
-                    if (err) {
-                        console.log('AMQP ERROR - Error asserting queue');
-                        return;
-                    }
-
-                    this.channel.bindQueue(q.queue, 'secondary', '',)
-
-                })
-
-
-            })
-        })
-    }
-
-    publishPrimary(message: any) {
-        this.channel.sendToQueue("primary23", Buffer.from(JSON.stringify(message)))
-    }
-
-    async publishSecondary(message: any) {
-        if (message.d.type === 3) {
-            const queue = await this.client.cache.get('event:message:' + message.d.message.id)
-            console.log(queue)
-
-            const data = { tag: 'event:message' + message.d.message.id, data: message }
-            this.channel.sendToQueue(queue, Buffer.from(JSON.stringify(data)))
+      this.channel.assertQueue('', { exclusive: true }, (err, q) => {
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+        if (err) {
+          console.log('AMQP ERROR - Error asserting queue')
+          return
         }
+
+        this.channel.bindQueue(q.queue, 'secondary', '')
+      })
+    })
+  }
+
+  publishPrimary (message: any): void {
+    console.log('published')
+    this.channel.sendToQueue('primary', Buffer.from(JSON.stringify(message)))
+  }
+
+  async publishSecondary (message: any): Promise<void> {
+    if (message.d.type === 3) {
+      const queue = await this.client.cache.get('event:message:' + String(message.d.message.id))
+      console.log(queue)
+
+      const data = { tag: 'event:message' + String(message.d.message.id), data: message }
+      this.channel.sendToQueue(queue, Buffer.from(JSON.stringify(data)))
     }
+  }
 }
--- src/messaging/RedisClient.ts
+++ src/messaging/RedisClient.ts
@@ -1,58 +1,56 @@
-import { BaseClass } from "../BaseClass";
-import { createClient, RedisClientType } from "redis";
-import { Gateway } from "../Gateway";
-import { DiscordAPIGuild, REDISOptions } from "@fawkes.js/api-types";
+import { BaseClass } from '../BaseClass'
+import { createClient, type RedisClientType } from 'redis'
+import { type Gateway } from '../Gateway'
+import { type DiscordAPIGuild, type REDISOptions } from '@fawkes.js/api-types'
 
 export class RedisClient extends BaseClass {
-    client: Gateway;
-    options: REDISOptions;
-    cache!: RedisClientType;
-    constructor(client: Gateway) {
-        super();
+  client: Gateway
+  options: REDISOptions
+  cache!: RedisClientType
+  constructor (client: Gateway) {
+    super()
 
-        this.options = client.options.redis;
+    this.options = client.options.redis
 
-        this.client = client;
+    this.client = client
 
-        Object.defineProperty(this, "cache", { value: null, writable: true });
-        Object.defineProperty(this, "subscriber", { value: null, writable: true });
+    Object.defineProperty(this, 'cache', { value: null, writable: true })
+    Object.defineProperty(this, 'subscriber', { value: null, writable: true })
+  }
 
-    }
+  async connect (): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    const url = this.options.url
+      ? this.options.url
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      : `redis://${this.options.username}:${this.options.password}@${this.options.hostname}:${this.options.port}`
 
-    async connect() {
-        const url = this.options.url
-            ? this.options.url
-            : `redis://${this.options.username}:${this.options.password}@${this.options.hostname}:${this.options.port}`;
+    this.cache = createClient({ url })
 
-        this.cache = createClient({ url });
+    await this.cache.connect()
+  }
 
-        await this.cache.connect();
-    }
+  async get (id: string): Promise<any> {
+    return JSON.parse(await this.cache.get(id) as string)
+  }
 
+  async set (id: string, data: any): Promise<any> {
+    return await this.cache.set(id, JSON.stringify(data))
+  }
 
+  async delete (id: string): Promise<any> {
+    const key = `guild:${id}`
+    return await this.cache.del(key)
+  }
 
-    async get(id: string) {
-        return JSON.parse(<string>await this.cache.get(id));
-    }
-    async set(id: string, data: any) {
-        return await this.cache.set(id, JSON.stringify(data));
-    }
+  async has (id: string): Promise<any> {
+    const key = `guild:${id}`
+    return await this.cache.get(key)
+  }
 
-    async delete(id: string) {
-        const key = `guild:${id}`;
-        return await this.cache.del(key);
-    }
+  async patch (id: string, data: DiscordAPIGuild): Promise<any> {
+    const key = `guild:${id}`
 
-    async has(id: string) {
-        const key = `guild:${id}`;
-        return (await this.cache.get(key)) ? true : false;
-    }
-
-    async patch(id: string, data: DiscordAPIGuild) {
-        const key = `guild:${id}`;
-
-        return await this.cache.set(key, JSON.stringify(data));
-    }
-
-
+    return await this.cache.set(key, JSON.stringify(data))
+  }
 }
