1beb57a70aebc3f57c10d3501d97f23bd2bcb6a1
Toby Chambers <74092151+thepiex@users.noreply.github.com>
2023-09-01 22:56:12 +0100

    Fix sharding

--- src/errors/FawkesError.ts
+++ src/errors/FawkesError.ts
@@ -3,7 +3,5 @@ export class FawkesError extends Error {
   description: string;
   constructor(code: string, description: string) {
     super(`\x1b[1m\x1b[91m[${code}]:\x1b[0m \x1b[10\x1b[91m${description}\x1b[0m`);
-    this.code = code;
-    this.description = description;
   }
 }
